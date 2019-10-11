68f2245b89339a62c846dc9e7a6598e6c84eec35
xiyu714 <35284881+xiyu714@users.noreply.github.com>
2019-10-12 01:06:08 +0800

    好像没有问题

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:paidui_xitong/state/now_paidui.dart';
 import 'paidui_history.dart';
 
 class Home extends StatefulWidget {
@@ -6,100 +8,110 @@ class Home extends StatefulWidget {
 }
 
 class _Home extends State<Home> {
+  NowPaidui paidui = NowPaidui();
+
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("主页"),
         ),
-      body: Column(
-
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.all(40),
-                child: Column(
-                  children: <Widget>[
-                    Text("当前排队人数"),
-                    Container(
-                      width: 90,
-                      height: 90,
-                      margin: EdgeInsets.only(top: 20),
-                      //alignment: Alignment.center,
-
-                      child: DecoratedBox(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            style: BorderStyle.solid
-                          ),
-                          borderRadius: BorderRadius.circular(45),
-
-                        ),
-                        child: Center(
-                          child: Text('re'),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.all(40),
-                child: Column(
-                  children: <Widget>[
-                    Text("预计等待时间"),
-                    Container(
-                      width: 90,
-                      height: 90,
-                      margin: EdgeInsets.only(top: 20),
-                      //alignment: Alignment.center,
-
-                      child: DecoratedBox(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                              style: BorderStyle.solid
-                          ),
-                          borderRadius: BorderRadius.circular(45),
-
-                        ),
-                        child: Center(
-                          child: Text('re'),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Container(
-            height: 45,
-            margin: EdgeInsets.symmetric(vertical: 10,horizontal: 20),
-            child: SizedBox.expand(
-              child: OutlineButton(
-                child: Text("查看排队最新进展"),
-                onPressed: () {},
-              ),
-            ),
-          ),
-          Container(
-            height: 45,
-            margin: EdgeInsets.symmetric(vertical: 10,horizontal: 20),
-            child: SizedBox.expand(
-              child: OutlineButton(
-                child: Text("查看历史数据"),
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(
-                    builder: (context) {
-                      return SimpleTimeSeriesChart.withSampleData();
-                    }
-                  ));
-                },
-              ),
-            ),
-          ),
+      body: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(builder: (_) {
+            paidui.run();
+            return paidui;
+          },)
         ],
+        child: Column(
+
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(40),
+                  child: Column(
+                    children: <Widget>[
+                      Text("当前排队人数"),
+                      Container(
+                        width: 90,
+                        height: 90,
+                        margin: EdgeInsets.only(top: 20),
+                        //alignment: Alignment.center,
+
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              style: BorderStyle.solid
+                            ),
+                            borderRadius: BorderRadius.circular(45),
+
+                          ),
+                          child: Center(
+                            child: Text('re'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(40),
+                  child: Column(
+                    children: <Widget>[
+                      Text("预计等待时间"),
+                      Container(
+                        width: 90,
+                        height: 90,
+                        margin: EdgeInsets.only(top: 20),
+                        //alignment: Alignment.center,
+
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                                style: BorderStyle.solid
+                            ),
+                            borderRadius: BorderRadius.circular(45),
+
+                          ),
+                          child: Center(
+                            child: Text('re'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              height: 45,
+              margin: EdgeInsets.symmetric(vertical: 10,horizontal: 20),
+              child: SizedBox.expand(
+                child: OutlineButton(
+                  child: Text("查看排队最新进展"),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+            Container(
+              height: 45,
+              margin: EdgeInsets.symmetric(vertical: 10,horizontal: 20),
+              child: SizedBox.expand(
+                child: OutlineButton(
+                  child: Text("查看历史数据"),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(
+                      builder: (context) {
+                        return SimpleTimeSeriesChart.withSampleData();
+                      }
+                    ));
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
