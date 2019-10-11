12a510e9f7987d258104ac79b6f2ebd2f1e02e14
xiyu714 <35284881+xiyu714@users.noreply.github.com>
2019-10-12 00:51:40 +0800

    创建paidui

---
+++ lib/state/now_paidui.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+/*
+  实现： 1.定时更新数据
+        2.立即更新数据
+        3.停止更新数据
+ */
+import 'package:flutter/material.dart';
+import 'package:paidui_xitong/model/paidui.dart';
+
+class NowPaidui with ChangeNotifier {
+  bool stop = false;
+  Paidui paidui;
+
+  run() {
+    const period = const Duration(seconds: 5);
+
+    Timer.periodic(period, (timer) async {
+      if(this.stop) { //stop时取消更新
+        timer.cancel();
+      } else {
+        this.paidui = await Paidui.post();
+        notifyListeners();
+      }
+    });
+  }
+}
