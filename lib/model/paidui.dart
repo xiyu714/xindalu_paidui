419e8d59ff67ad96f8795687f169c86f39e015b3
xiyu714 <35284881+xiyu714@users.noreply.github.com>
2019-10-11 18:09:00 +0800

    等下再修改_ResultObj_history

--- lib/model/paidui.dart
+++ lib/model/paidui.dart
@@ -36,3 +36,39 @@ class _ResultObj {
   factory _ResultObj.fromJson(Map<String, dynamic> json) => _$_ResultObjFromJson(json);
   Map<String, dynamic> toJson() => _$_ResultObjToJson(this);
 }
+
+@JsonSerializable()
+class Paidui_history {
+  int Status;
+  int StatusCode;
+  String Msg;
+  var ErrorObj;
+  _ResultObj_history ResultObj;
+
+  Paidui_history();
+
+  static Future<Paidui_history> post() async {
+    var dio = Dio(BaseOptions(
+        headers: {
+          "AccessToken": AccessToken,
+        }
+    ));
+    var response = await dio.get("http://api.nlecloud.com/devices/44443/datas?apitags=number_down", queryParameters: {
+      "Method": 1,
+      "TimeAgo": 5,
+    });
+    //print(response);
+    return Paidui_history.fromJson(response.data);
+  }
+
+  factory Paidui_history.fromJson(Map<String, dynamic> json) => _$Paidui_historyFromJson(json);
+  Map<String, dynamic> toJson() => _$Paidui_historyToJson(this);
+}
+
+@JsonSerializable()
+class _ResultObj_history {
+  _ResultObj_history();
+
+  factory _ResultObj_history.fromJson(Map<String, dynamic> json) => _$_ResultObj_historyFromJson(json);
+  Map<String, dynamic> toJson() => _$_ResultObj_historyToJson(this);
+}
--- lib/model/paidui.g.dart
+++ lib/model/paidui.g.dart
@@ -31,3 +31,31 @@ _ResultObj _$_ResultObjFromJson(Map<String, dynamic> json) {
 
 Map<String, dynamic> _$_ResultObjToJson(_ResultObj instance) =>
     <String, dynamic>{'Value': instance.Value};
+
+Paidui_history _$Paidui_historyFromJson(Map<String, dynamic> json) {
+  return Paidui_history()
+    ..Status = json['Status'] as int
+    ..StatusCode = json['StatusCode'] as int
+    ..Msg = json['Msg'] as String
+    ..ErrorObj = json['ErrorObj']
+    ..ResultObj = json['ResultObj'] == null
+        ? null
+        : _ResultObj_history.fromJson(
+            json['ResultObj'] as Map<String, dynamic>);
+}
+
+Map<String, dynamic> _$Paidui_historyToJson(Paidui_history instance) =>
+    <String, dynamic>{
+      'Status': instance.Status,
+      'StatusCode': instance.StatusCode,
+      'Msg': instance.Msg,
+      'ErrorObj': instance.ErrorObj,
+      'ResultObj': instance.ResultObj
+    };
+
+_ResultObj_history _$_ResultObj_historyFromJson(Map<String, dynamic> json) {
+  return _ResultObj_history();
+}
+
+Map<String, dynamic> _$_ResultObj_historyToJson(_ResultObj_history instance) =>
+    <String, dynamic>{};
