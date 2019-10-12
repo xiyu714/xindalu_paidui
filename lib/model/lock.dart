cee8915dc4f5c003ceaee54fb03f9984847895bb
xiyu714 <35284881+xiyu714@users.noreply.github.com>
2019-10-12 17:42:30 +0800

    f

--- lib/model/lock.dart
+++ lib/model/lock.dart
@@ -18,7 +18,7 @@ class Lock {
           "AccessToken": AccessToken,
         }
     ));
-    var response = await dio.get("http://api.nlecloud.com/Cmds", queryParameters: {
+    var response = await dio.post("http://api.nlecloud.com/Cmds", queryParameters: {
       "deviceId": 44443,
       "apiTag": "lock",  // 想设为5分钟，为了方便调试先设为60
     });
