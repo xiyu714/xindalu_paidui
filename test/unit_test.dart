c485dd8e8831812f46175271edf2ed2a815813d9
xiyu714 <35284881+xiyu714@users.noreply.github.com>
2019-10-12 17:40:22 +0800

    添加测试

---
+++ test/unit_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'utils.dart';
+
+import 'package:paidui_xitong/model/lock.dart';
+import 'dart:convert';
+
+void main() {
+  group("lock 相关", () {
+    test("lock", () async {
+      await util_login();
+
+      var data = await Lock.post();
+      print(jsonEncode(data));
+    });
+  });
+}
